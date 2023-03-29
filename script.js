4c8f782514a7163b3079e6ec6d77f6f1b860f9b0
EdgardCJr <edgardcastanhojr@hotmail.com>
2023-03-29 16:43:02 -0300

    Alterações 2

--- script.js
+++ script.js
@@ -18,15 +18,14 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".city").innerText = "Clima em " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
+      "Humidade: " + humidity + "%";
     document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
+      "Velocidade do vento: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
@@ -48,4 +47,4 @@ document
     }
   });
 
-weather.fetchWeather("Denver");
+weather.fetchWeather("São Paulo");
