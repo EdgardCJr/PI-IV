fc2d0e09da2c1e38af330ce8b92cedcc8e48701e
EdgardCJr <edgardcastanhojr@hotmail.com>
2023-05-29 21:17:39 -0300

    Alteração de Pasta

--- Teste1/script.js
+++ Teste1/script.js
@@ -2,7 +2,7 @@ let weather = {
   apiKey: "c001b63e7af7f063a16d3160c0cec6e0",
   fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=metric&appid=" + this.apiKey + "&lang=pt-br" 
+      "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=metric&appid=" + this.apiKey + "&lang=pt_br" 
     )
       .then((response) => {
         if (!response.ok) {
@@ -17,7 +17,7 @@ let weather = {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
-    const { speed } = data.wind;
+    const { speed,gust } = data.wind;
     document.querySelector(".city").innerText = "Clima em " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
@@ -25,7 +25,9 @@ let weather = {
     document.querySelector(".humidity").innerText =
       "Humidade: " + humidity + "%";
     document.querySelector(".wind").innerText =
-      "Velocidade do vento: " + speed + " km/h";
+    "Velocidade do vento: " + speed + " Km/h";
+    document.querySelector(".wind").innerText = 
+    "Nivel de Particulado: " + gust;
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
@@ -34,11 +36,9 @@ let weather = {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
-
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
-
 document
   .querySelector(".search-bar")
   .addEventListener("keyup", function (event) {
@@ -46,5 +46,4 @@ document
       weather.search();
     }
   });
-
 weather.fetchWeather("São Paulo");
